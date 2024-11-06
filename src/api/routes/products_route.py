f688d129a05bfc4f5f139e0876dd8168f2a2cd1d
Bryan Duarte <1878bj@gmail.com>
2024-11-06 00:12:56 -0300

    [update] queda funcionales los endpoints

--- src/api/routes/products_route.py
+++ src/api/routes/products_route.py
@@ -2,7 +2,6 @@ from flask import Blueprint, jsonify, request
 from src.services.products_service import ProductsService
 
 products_bp = Blueprint('products', __name__)
-# La validación de los datos de entrada ya está hecha por API Gateway
 
 @products_bp.route('/<int:id>', methods=['GET'])
 def get_product(id):
--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -2,10 +2,11 @@ import awsgi
 from .app import app
 from aws_lambda_powertools import Tracer, Logger, Metrics
 from aws_lambda_powertools.metrics import MetricUnit
+import traceback
 
 tracer = Tracer()
 logger = Logger()
-metrics = Metrics()
+metrics = Metrics(namespace="FlaskAPIMetrics")
 
 @tracer.capture_lambda_handler
 @metrics.log_metrics
@@ -13,14 +14,10 @@ metrics = Metrics()
 def lambda_handler(event, context):
     try:
         metrics.add_metric(name="api_requests", unit=MetricUnit.Count, value=1)
-        
-        if context.get("cold_start", True):
-            logger.info("Cold start detected")
-            metrics.add_metric(name="cold_starts", unit=MetricUnit.Count, value=1)
-
         response = awsgi.response(app, event, context)
         return response
     except Exception as e:
         metrics.add_metric(name="api_errors", unit=MetricUnit.Count, value=1)
         logger.error(f"Error procesando request: {e}")
+        logger.error(traceback.format_exc())
         raise
