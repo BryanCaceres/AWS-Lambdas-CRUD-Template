2314267223df679fc6f31204b6be60d202cd63cb
Bryan Duarte <1878bj@gmail.com>
2024-11-17 13:08:51 -0300

    [update] se ajustan los nuevos endpoints

--- src/api/routes/products_route.py
+++ src/api/routes/products_route.py
@@ -3,17 +3,17 @@ from src.services.products_service import ProductsService
 
 products_bp = Blueprint('products', __name__)
 
-@products_bp.route('/<string:id>', methods=['GET'])
-def get_product(id):
+@products_bp.route('/', methods=['GET'])
+def get_all_products():
     products_service = ProductsService()
-    product = products_service.get_product_by_pk(id)
-    return jsonify(message=product)
+    result = products_service.get_products()
+    return jsonify(result)
 
-@products_bp.route('/<string:id>', methods=['PATCH'])
-def update_product(id):
+@products_bp.route('/<string:pk>', methods=['GET'])
+def get_product(pk):
     products_service = ProductsService()
-    product = products_service.update_product(id, request.json)
-    return jsonify(message=product), 201
+    product = products_service.get_product_by_pk(pk)
+    return jsonify(message=product)
 
 @products_bp.route('/', methods=['POST'])
 def create_product():
@@ -21,14 +21,14 @@ def create_product():
     product = products_service.create_product(request.json)
     return jsonify(message=product), 201
 
-@products_bp.route('/', methods=['GET'])
-def get_all_products():
+@products_bp.route('/<string:pk>', methods=['PATCH'])
+def update_product(pk):
     products_service = ProductsService()
-    result = products_service.get_products()
-    return jsonify(result)
+    product = products_service.update_product(pk, request.json)
+    return jsonify(message=product), 201
 
-@products_bp.route('/<string:id>', methods=['DELETE'])
-def delete_product(id):
+@products_bp.route('/<string:pk>', methods=['DELETE'])
+def delete_product(pk):
     products_service = ProductsService()
-    result = products_service.delete_product(id)
+    result = products_service.delete_product(pk)
     return jsonify(result)
