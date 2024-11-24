bcf081a6a621943216dc06eb1849c714af4e7185
Bryan Duarte <55006434+BryanCaceres@users.noreply.github.com>
2024-11-24 19:10:40 -0300

    25 validation schemas (#26)
    * [update] se ajustan modelos de validación de entrada
    
    * [update] se anade tabulacion por punteros y limit en get all

--- lambda-layers/common/python/lambda_utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .decorators import lambda_handler_decorator
-
-__all__ = [
-    "lambda_handler_decorator"
-]
--- src/get/get_all/get_service.py
+++ src/get/get_all/get_service.py
@@ -8,16 +8,31 @@ class GetService:
         self.dynamodb = boto3.resource('dynamodb', region_name=settings.dynamodb_region_name)
         self.table = self.dynamodb.Table(settings.products_table)
 
-    def get_all(self) -> Optional[Dict]:
+    def get_all(self, last_evaluated_key: Optional[Dict] = None, limit: int = 50) -> Dict:
         """
         Gets all products from DynamoDB
+        :params:
+            last_evaluated_key: Last evaluated key from previous scan
+            limit: Limit of products to return
         :return: List of products in table products
         """
         try:
-            response = self.table.scan()
-            products = response.get('Items', [])            
-            return products
-        
+            params = {
+                'Limit': limit,
+                'ReturnConsumedCapacity': 'TOTAL'
+            }
+            
+            if last_evaluated_key:
+                params['ExclusiveStartKey'] = last_evaluated_key
+            
+            response = self.table.scan(**params)
+            
+            return {
+                'items': response.get('Items', []),
+                'last_evaluated_key': response.get('LastEvaluatedKey'),
+                'consumed_capacity': response.get('ConsumedCapacity')
+            }
+
         except ClientError as e:
             error_code = e.response.get('Error', {}).get('Code', 'UnknownError')
             error_message = e.response.get('Error', {}).get('Message', 'An unexpected error occurred.')
@@ -27,3 +42,4 @@ class GetService:
                 error_message = settings.default_error_message
 
             raise APIException(f"Error getting all products: {error_message}", status_code=int(error_code))
+        
