f59a19499fd4ef3beeefb077601e997aa5c5426b
Bryan Duarte <55006434+BryanCaceres@users.noreply.github.com>
2024-11-25 17:05:52 -0300

    [add] se añade uso de parameter storage para config de las lambdas (#28)

--- lambda-layers/common/python/lambda_custom_layer/core/settings.py
+++ lambda-layers/common/python/lambda_custom_layer/core/settings.py
@@ -1,9 +1,21 @@
+import boto3
+import json
 
 class Settings:
     def __init__(self):
-        self.dynamodb_region_name = 'us-east-1'
-        self.products_table = 'products'
-        self.default_error_code = '500'
-        self.default_error_message = 'Internal server error. Please try again later.'
+        ssm = boto3.client('ssm', region_name='us-east-1')
+
+        raw_parameters = ssm.get_parameter(
+            Name='/lambda-crud-api/config/prod',
+            WithDecryption=True
+        )
+        parameters = json.loads(raw_parameters['Parameter']['Value'])
+
+        self.dynamodb_region_name = parameters.get('dynamodb_region_name', 'us-east-1')
+        self.products_table = parameters.get('products_table', 'products')
+        self.default_error_code = parameters.get('default_error_code', '500')
+        self.default_error_message = parameters.get('default_error_message', 'Internal server error. Please try again later.')
+        self.default_limit = parameters.get('default_limit', 50)
+        self.log_level = parameters.get('log_level', 'ERROR')
 
 settings = Settings()
--- src/get/get_all/get_handler.py
+++ src/get/get_all/get_handler.py
@@ -1,5 +1,5 @@
 from typing import Dict, Optional, Any
-from lambda_custom_layer import lambda_handler_decorator
+from lambda_custom_layer import lambda_handler_decorator, settings
 from get_service import GetService
 import json
 
@@ -7,15 +7,17 @@ service = GetService()
 
 @lambda_handler_decorator
 def lambda_handler(event: Dict, context: Any) -> Dict:
-    limit = int(event.get('queryStringParameters', {}).get('limit', 50))
+    query_params = event.get('queryStringParameters') or {}
     
-    last_evaluated_key_str = event.get('queryStringParameters', {}).get('last_evaluated_key', None)
+    limit = int(query_params.get('limit', settings.default_limit))
+    
+    last_evaluated_key_str = query_params.get('last_evaluated_key')
     last_evaluated_key = None
     if last_evaluated_key_str and last_evaluated_key_str.lower() not in ['null', 'none', '']:
         try:
             last_evaluated_key = json.loads(last_evaluated_key_str)
         except json.JSONDecodeError:
-            last_evaluated_key = None
+            last_evaluated_key = {}
 
     products = service.get_all(last_evaluated_key, limit)
 
